ce136aedd81bfb7a03877283d23334801b3bf1ca
MatthewShindel <matthew@shindel.com>
2023-09-19 14:29:00 -0400

    remake for_challenge for video

--- for_challenge.js
+++ for_challenge.js
@@ -1,6 +1,7 @@
 // Challenge 3
 // Use a for statement to log to the console only the words that are exactly 3 characters in length
 var words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
+
 for (var i = 0; i < words.length; i++){
     if(words[i].length === 3){
         console.log(words[i]);
